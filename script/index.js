5f32d85e23b88d3d2b6830513216d66189a4dedf
Alexander Ustinov <ustomsk@yandex.ru>
2022-01-16 16:29:59 +0600

    5 проект

--- script/index.js
+++ script/index.js
@@ -1,159 +1,99 @@
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const placeAddButton = profile.querySelector('.profile__add-button');
+const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector(".popup_type_edit");
+const popupAddCard = document.querySelector('.popup_type_add-card');
+const popupContainer = popup.querySelector('.popup__container');
+const popupCloseButton = popupEditProfile.querySelector('.popup__close');
+const addPopupCloseButton = popupAddCard.querySelector('.popup__close');
+const modalCardView = document.querySelector('.popup_type_image-container');
+const cardViewCloseBtn = modalCardView.querySelector('.popup__close');
+const formEditProfile = popupEditProfile.querySelector('.popup-form');
+const addPopupImageName = document.querySelector('.popup__place-name');
+const addPopupImagelink = modalCardView.querySelector('.popup__image-link');
+const formAddCard = popupAddCard.querySelector('.popup-form');
+const inputName = document.querySelector(".popup-form__input_card-name");
+const inputLink = document.querySelector(".popup-form__input_card-link");
+const nameInput = document.querySelector('.popup-form__input_name');
+const jobInput = document.querySelector('.popup-form__input_job');
+const profileTitle = profile.querySelector('.profile__title');
+const profileSubtitle = profile.querySelector('.profile__subtitle');
 
-const editModal = document.querySelector(".popup_type_edit");
-const addCardModal = document.querySelector(".popup_type_add-card");
-//const viewCardModal = document.querySelector('.popup_type_image-container');
-// CloseViewCardModal = viewCardModal.querySelector('.popup__close');
 
-const closeAddCardModalButton = addCardModal.querySelector('.popup__close');
-const closeEditModal = editModal.querySelector('.popup__close');
-const editProfileButton = document.querySelector('.profile__edit-button');
-const addCardButton = document.querySelector('.profile__add-button');
-//const viewCardButton = document.querySelector('.card__view-button');
-
-const inputName = document.querySelector(".popup__input_type_card-name");
-const inputLink = document.querySelector(".popup__input_card-link");
-
-const editForm = editModal.querySelector('.popup__form');
-const addCardForm = addCardModal.querySelector('.popup__form');
-
-addCardForm.addEventListener('submit', (event) => {
-  event.preventDefault()
-
-  const nameValue = inputName.value;
-  const linkValue = inputLink.value;
-
-  createCard({
-    name: nameValue,
-    link: linkValue
-  })
-  toogleModal(addCardModal);
+formEditProfile.addEventListener('submit', (event) => {
+    event.preventDefault()
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    toogleModal(popupEditProfile);
 });
 
-editForm.addEventListener('submit', (event) => {
-  event.preventDefault()
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  toogleModal(editModal);
+formAddCard.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const nameValue = inputName.value;
+    const linkValue = inputLink.value;
+    document.placeForm.reset();
+    renderCard({
+        name: nameValue,
+        link: linkValue
+    })
+
+    toogleModal(popupAddCard);
 });
-
-
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_job');
-
-
 
 function toogleModal(modal) {
 
-  modal.classList.toggle('popup_opened')
+    modal.classList.toggle('popup_opened')
 
 }
-editProfileButton.addEventListener('click', () => toogleModal(editModal))
-closeEditModal.addEventListener('click', () => toogleModal(editModal))
+profileEditButton.addEventListener('click', () => toogleModal(popupEditProfile))
+popupCloseButton.addEventListener('click', () => toogleModal(popupEditProfile))
 
-addCardButton.addEventListener('click', () => toogleModal(addCardModal))
-closeAddCardModalButton.addEventListener('click', () => toogleModal(addCardModal))
+placeAddButton.addEventListener('click', () => toogleModal(popupAddCard))
+addPopupCloseButton.addEventListener('click', () => toogleModal(popupAddCard))
 
-//viewCardButton.addEventListener('click', () => toogleModal(viewCardModal))
-//CloseViewCardModal.addEventListener('click', () => toogleModal(viewCardModal))
+cardViewCloseBtn.addEventListener('click', (event) => toogleModal(modalCardView))
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1630354102354-2471c8851e60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const list = document.querySelector(".elements");
-const cardTemplate = document.querySelector('.card__template').content;
-
-//удаление карточки
-function deleteHandler(e) {
-  e.target.closest('.element').remove()
-  cardElement.remove()
-}
-
-
+const cardsList = document.querySelector(".gallery");
+const cardTemplate = document.querySelector('.card-template').content;
 
 function createCard(cardData) {
-  console.log("cardData", cardData);
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardImage = cardElement.querySelector('.element__image');
-  const cardTitle = cardElement.querySelector('.element__group-title');
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  const likeButton = cardElement.querySelector('.element__group-heart')
-  const viewCardButton = cardElement.querySelector('.element__view-button');
-  cardTitle.textContent = cardData.name;
-  cardImage.src = `${cardData.link}`;
-  console.log("element__image", cardImage);
-  console.log("element__group-title", cardTitle);
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector('.gallery__item');
+    const cardTitle = cardElement.querySelector('.gallery__title');
+    const deleteButton = cardElement.querySelector('.gallery__delete-button');
+    const likeButton = cardElement.querySelector('.gallery__button')
+    const viewCardButton = cardElement.querySelector('.gallery__view-button');
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
 
+    function openPopupImage(modalCardView) {
+        addPopupImageName.textContent = cardTitle.textContent;
+        addPopupImagelink.src = cardImage.src;
+        toogleModal(modalCardView);
+    }
 
-  deleteButton.addEventListener("click", deleteHandler)
-  likeButton.addEventListener("click", likeClickHandler)
-  viewCardButton.addEventListener('click', () => openPopupImage(viewCardModal))
+    function likeClickHandler() {
+        likeButton.classList.toggle('gallery__button_active');
+    }
+    //удаление карточек
+    function deleteHandler(e) {
+        e.target.closest('.gallery__cart').remove()
+    }
 
-  function likeClickHandler() {
-    likeButton.classList.toggle('element__group-heart_active');
-  }
+    deleteButton.addEventListener("click", deleteHandler)
+    likeButton.addEventListener("click", likeClickHandler)
+    viewCardButton.addEventListener('click', () => openPopupImage(modalCardView))
 
-
-  list.prepend(cardElement);
+    cardsList.prepend(cardElement);
+    return cardElement;
 }
 
-initialCards.forEach(createCard);
+function renderCard(cardData) {
+    const cardElement = createCard(cardData);
+    cardsList.prepend(cardElement);
+}
 
-// function openPopupEdit() {
-//   editModal.classList.add('popup_opened');
-//   nameInput.value = profileName.textContent;
-//   jobInput.value = profileJob.textContent;
-// }
-//
-//
-// function openPopupAdd() {
-//   addCardModal.classList.add('popup_opened');
-// }
-//
-// function closePopup() {
-//   editModal.classList.remove('popup_opened');
-// }
-//
-// function formSubmitHandler(evt) {
-//   evt.preventDefault();
-//   profileName.textContent = nameInput.value;
-//   profileJob.textContent = jobInput.value;
-//   closePopup();
-// }
-//
-// formElement.addEventListener('submit', formSubmitHandler);
-// popupOpenButton.addEventListener('click', openPopupEdit);
-// popupOpenButtonAdd.addEventListener('click', openPopupAdd);
-// popupCloseButton.addEventListener('click', closePopup);
+initialCards.forEach(renderCard);
 
-//FormData.reset();
-//
-//очистить форму добавления карточек
-//вернуть базовые значения профиля
-//сделать третье модальное окно.
-//Плавное открытие и закрытие попапов
