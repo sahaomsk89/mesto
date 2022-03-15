c87af3c0ff3b688cfd6ef8d59d5c577f0b75aca3
Alexander Ustinov <ustomsk@yandex.ru>
2022-03-15 09:05:18 +0600

    8 проект кор

---
+++ src/components/Api.js
@@ -0,0 +1,51 @@
+class Api {
+    constructor({ baseUrl, headers }) {
+        this._baseUrl = baseUrl;
+        this._headers = headers;
+    }
+
+    getProfile() {
+        console.log('getProfile')
+        return fetch(`${this._baseUrl}users/me`, {
+            headers: this._headers
+        })
+            .then(res => res.ok ? res.json() : Promise.reject(res.status))
+            .catch(console.log)
+
+
+    }
+
+    getInitialCards() {
+        return fetch(`${this._baseUrl}/cards `, {
+            headers: this._headers
+        })
+            .then(res => res.ok ? res.json() : Promise.reject(res.status))
+            .catch(console.log)
+
+    }
+
+    editProfile() {
+        return fetch(`${this._baseUrl}users/me`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({
+                name: 'Marie Skłodowska Curie',
+                about: 'Physicist and Chemist'
+            })
+        })
+            .then(res => res.ok ? res.json() : Promise.reject(res.status))
+            .catch(console.log)
+
+    }
+
+
+
+}
+
+export const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-37',
+    headers: {
+        authorization: 'd0911b03-b62b-4bf7-b304-82194d9acc6e',
+        'Content-Type': 'application/json'
+    }
+}); 
